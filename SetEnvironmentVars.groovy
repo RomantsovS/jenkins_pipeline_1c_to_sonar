2f725542e6c62e115416bf0b1b2dfad72938656f
RomantsovS <romantsov_s@rusklimat.ru>
2020-11-18 20:39:54 +0300

    env.ACC_USER = "Administrator"

--- SetEnvironmentVars.groovy
+++ SetEnvironmentVars.groovy
@@ -7,10 +7,6 @@ env.sonar_catalog = "C:/Sonar"
 env.PROPERTIES_CATALOG = "./sonar"
 env.STEBI_SETTINGS = ""
 
-env.ACC_BASE_NAME = "AutoConfigurationCheck"
-env.ACC_BASE_SERVER1C = "cv8app12:1741"
-env.ACC_USER = "Administrator"
-
 env.TIMEOUT_FOR_CHECKOUT_STAGE = "5"
 
 
@@ -19,3 +15,7 @@ env.TIMEOUT_FOR_ACC_STAGE = "60"
 
 env.ACC_PROPERTIES = "./acc.properties"
 env.BSL_LS_PROPERTIES = "./bsl-language-server.conf"
+
+env.ACC_BASE_NAME = "AutoConfigurationCheck"
+env.ACC_BASE_SERVER1C = "cv8app12:1741"
+env.ACC_USER = "Administrator"

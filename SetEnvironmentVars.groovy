01e60a9d658e20a1f95715348085e55ccd2725d8
RomantsovS <romantsov_s@rusklimat.ru>
2020-11-18 18:44:02 +0300

    env.git_repo_url = "e:/SonarQube/PDE"

--- SetEnvironmentVars.groovy
+++ SetEnvironmentVars.groovy
@@ -2,7 +2,7 @@ env.VERBOSE = "true"
 env.EMAIL_ADDRESS_FOR_NOTIFICATIONS = "romantsov_s@rusklimat.ru"
 
 env.PROJECT_NAME = "PDE"
-env.git_repo_url = "e:\SonarQube\PDE"
+env.git_repo_url = "e:/SonarQube/PDE"
 env.git_repo_branch = "master"
 env.sonar_catalog = "C:/Sonar"
 env.PROPERTIES_CATALOG = ""

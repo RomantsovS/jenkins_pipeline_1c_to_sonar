8b975fb3fd0ab74866122a05a37472548d6b624b
RomantsovS <romantsov_s@rusklimat.ru>
2020-11-23 21:27:30 +0300

    deleted env.PROJECT_NAME

--- SetEnvironmentVars.groovy
+++ SetEnvironmentVars.groovy
@@ -1,7 +1,6 @@
 env.VERBOSE = "true"
 env.EMAIL_ADDRESS_FOR_NOTIFICATIONS = "romantsov_s@rusklimat.ru"
 
-env.PROJECT_NAME = "PDE"
 env.git_repo_branch = "master"
 env.sonar_catalog = "C:/Sonar"
 env.JAVA_11_HOME = sonar_catalog + "/jdk-11.0.2"
@@ -9,7 +8,6 @@ env.PROPERTIES_CATALOG = "./sonar"
 env.STEBI_SETTINGS = ""
 
 env.TIMEOUT_FOR_CHECKOUT_STAGE = "5"
-
 
 //параметры для переопределения
 env.TIMEOUT_FOR_ACC_STAGE = "60"
